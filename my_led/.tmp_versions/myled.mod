5602d6d2de260ceea47989a21b0634ddaaf570a5
Oshimo Yasuaki <syokkee@gmail.com>
2019-12-29 19:15:34 +0900

    Add a file

--- my_led/.tmp_versions/myled.mod
+++
@@ -1,3 +0,0 @@
-/home/pi/my_led/myled.ko
-/home/pi/my_led/myled.o
-
--- my_led/tika.bash
+++
@@ -1,9 +0,0 @@
-sudo chmod 666 /dev/myled0 
-
-echo 1 > /dev/myled0
-sleep 1s
-echo 0 > /dev/myled0
-sleep 5s
-echo 1 > /dev/myled0
-sleep 2s
-echo 0 > /dev/myled0
